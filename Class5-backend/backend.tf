36757cf5761b361d1936fe3ec4ae39942b6ad40e
JackieAisa <aisakhunova@gmail.com>
2025-01-23 03:07:48 +0000

    new file

--- Class5-backend/backend.tf
+++ Class5-backend/backend.tf
@@ -1,8 +1,14 @@
-terraform {
-  backend "s3" {
-    bucket = "kaizen-jackiea"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "lock-state"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "kaizen-jackiea"
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"
+#     dynamodb_table = "lock-state"
+#   }
+# }
+
+
+#1. Store in remote backend
+#2. Version statefile
+#3. Lock statefile
+#4. Secure it (access)
